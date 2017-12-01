3489459d39f0839377633d26d76934539d499c03
Peter Buchardt <buchardt@gmail.com>
2017-12-01 16:49:52 +0000

    added audio
    audio!

---
+++ Assets/Scripts/MainCharAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharAudio : MonoBehaviour {
+	public AudioSource spaceship;
+	public Rigidbody ship;
+
+	float speed = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float volLerp = Mathf.InverseLerp(0.5f, 3.75f, ship.velocity.magnitude);
+		spaceship.volume = volLerp;
+	}
+}
