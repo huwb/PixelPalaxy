0f1bfbbe3888af5db0e89d021da3fa8dfd369d0e
Alastair <addyfilm@yahoo.com>
2017-12-01 19:31:19 +0000

    Start adding score

---
+++ Assets/PixelScoreAdder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelScoreAdder : MonoBehaviour {
+    private List<GameObject> _pixelsBeenHit;
+    private MakePixels _makePixels;
+
+	// Use this for initialization
+	void Start () {
+        _makePixels = GameObject.Find("PlayerCharacter").GetComponent<MakePixels>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(GameObject o in _pixelsBeenHit)
+        {
+            if( other.gameObject == o)
+            {
+                return;
+            }
+        }
+        _pixelsBeenHit.Add(other.gameObject);
+        _makePixels.AddToScore(10);
+
+    }
+}
