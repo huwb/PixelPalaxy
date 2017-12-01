42024b6c215c7e76127ee646d906105704843524
huwb <huw.bowles@gmail.com>
2017-12-01 13:56:06 +0000

    Added player control script

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float _force = 1f;
+
+    Rigidbody _rb;
+
+    void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+    {
+        _rb.AddForce( _force * Vector3.right * Input.GetAxis( "Horizontal" ), ForceMode.Impulse );
+        _rb.AddForce( _force * Vector3.up * Input.GetAxis( "Vertical" ), ForceMode.Impulse );
+	}
+}
