5f05286abb598f9ea40eb1d58a08c1ec440b4a3c
huwb <huw.bowles@gmail.com>
2017-12-01 13:19:09 +0000

    Initial commit

---
+++ Assets/MakePixels.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePixels : MonoBehaviour
+{
+    public float _attractForce = 100f;
+    public float _attractRadius = 2f;
+    public float _charAttractForce = 1f;
+
+    public float _pixelSize = 0.1f;
+
+    public Transform _pixelPrefab;
+
+    public List<Rigidbody> _pixels = new List<Rigidbody>();
+
+	void Start ()
+    {
+        int N = 10;
+        for( int i = 0; i < N; i++ )
+        {
+            for( int j = 0; j < N; j++ )
+            {
+                Transform inst = Instantiate( _pixelPrefab ) as Transform;
+                inst.position = transform.position + new Vector3( (i - N / 2) * _pixelSize, (j - N / 2) * _pixelSize, 0f );
+                inst.GetComponent<Renderer>().material.color = Random.ColorHSV( 0f, 1f, 0.25f, 1f, 1f, 1f );
+                //inst.parent = transform;
+                _pixels.Add( inst.GetComponent<Rigidbody>() );
+            }
+        }
+    }
+	
+	void FixedUpdate ()
+    {
+        Pixel.AttractPixelsTowardsRB( this, GetComponent<Rigidbody>(), false, _charAttractForce );
+	}
+}
---
+++ Assets/Pixel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixel : MonoBehaviour
+{
+    MakePixels _mp;
+
+    private void Start()
+    {
+        _mp = FindObjectOfType<MakePixels>();
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        AttractPixelsTowardsRB( _mp, rb, true, _mp._attractForce );
+    }
+
+    public static void AttractPixelsTowardsRB( MakePixels mp, Rigidbody attractTo, bool useRad, float force )
+    {
+        float rmax = mp._attractRadius * mp._pixelSize;
+        foreach( Rigidbody rb in mp._pixels )
+        {
+            if( rb == attractTo ) continue;
+
+            Vector3 offset = (attractTo.transform.position - rb.transform.position);
+            float l2 = offset.sqrMagnitude;
+            if( (useRad && l2 > rmax * rmax) )
+                continue;
+            if( l2 <= Mathf.Epsilon )
+                continue;
+
+            l2 = Mathf.Sqrt( l2 );
+            offset /= l2;
+
+            rb.AddForce( offset * force, ForceMode.Impulse );
+            attractTo.AddForce( -offset * force, ForceMode.Impulse );
+        }
+    }
+}
